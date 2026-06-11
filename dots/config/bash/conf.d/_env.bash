4f35ec85106330724a7f5569441e40c9a20a9b1b
mkunten <mkunten+github@gmail.com>
2026-06-11 11:46:30 +0900

    fix: add closing quot

--- dots/config/bash/conf.d/_env.bash
+++ dots/config/bash/conf.d/_env.bash
@@ -4,7 +4,7 @@
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_STATE_HOME="$HOME/.local/state
+export XDG_STATE_HOME="$HOME/.local/state"
 
 # go
 export GOPATH="$XDG_DATA_HOME/go"
